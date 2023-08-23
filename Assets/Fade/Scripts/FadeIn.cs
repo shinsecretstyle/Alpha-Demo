9a19f9e69bf9dd80255d2db3eb591ba2603ac98e
Shunlinux <shun0623.kuw@gmail.com>
2023-08-23 15:43:37 +0900

    fade in

---
+++ Assets/Fade/Scripts/FadeIn.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] Fade fade;
+    public void NextScene()
+    {
+        Action act = () => SceneManager.LoadScene("GameClear");
+        float time = 1f;
+
+        //1秒間フェードしてからにシーン移動する
+        fade.FadeIn(time, act);
+    }
+}
