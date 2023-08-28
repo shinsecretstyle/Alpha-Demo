04086316b4018491c63952e367547d40d0f84592
Shunlinux <shun0623.kuw@gmail.com>
2023-08-29 02:30:45 +0900

    se

---
+++ Assets/Scripts/Lovescripts/Select.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+    private AudioSource sound01;
+    private AudioSource sound02;
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound01 = audioSources[0];
+        sound02 = audioSources[1];
+    }
+    public void OnClick()
+    {
+        sound01.PlayOneShot(sound01.clip);
+    }
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) ||
+            Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            sound02.PlayOneShot(sound02.clip);
+        }
+    }*/
+}
